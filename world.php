595f770bd8c5ca742e50e0f58fc91fb3b9647a3e
LEGENDARY <tajay59@live.com>
2021-11-09 21:35:30 -0500

    Exercise 4 finished

--- world.php
+++ world.php
@@ -14,9 +14,25 @@ $results 	  = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+
+
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+
+  <?php foreach ($results as $row): ?>
+    <tr>
+    <td><?= $row['name']?></td>
+    <td><?= $row['continent']?></td>
+    <td><?= $row['independence_year']?></td>
+    <td><?= $row['head_of_state']?></td>
+  </tr>
 <?php endforeach; ?>
-</ul>
+  
+</table>
 -
